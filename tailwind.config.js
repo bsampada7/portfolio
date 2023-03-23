9feaa0ccc0581fde7b5166d3c0f5b676bc6badde
bhujels <bhujelsampada@gmail.com>
2023-03-22 22:40:08 -0400

    add force-graph and send svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,33 @@ module.exports = {
         primary: "#8401ff",
         "primary-light": "#b566ff",
         "primary-dark": "#7700e6",
+
+        color1: "#EEB4B3",
+        color2: "#C179B9",
+        color3: "#A42CD6",
+        color4: "#502274",
+        color5: "#2F242C",
+
+        "linear-grad": "linear-gradient(#e000ff 0%, #8401ff 100%)",
+
+        primary: "#4717F1",
+        "primary-dark": "#350cca",
+        secondary: "#B417F1",
+        background: "#0E0B16",
+        background1: "#0B0D16",
+        background2: "hsla(0, 0%, 22%,0.4)",
+        "text-muted": "#F5E2C8",
+        tertiary: "#D88373",
+        text: "#F7F7F7",
       },
       fontFamily: {
-        handwriting: ["sacramento"],
+        // handwriting: ["sacramento"],
       },
       fontSize: {
         xxs: "0.5rem",
+      },
+      borderRadius: {
+        half: "50%",
       },
     },
   },
