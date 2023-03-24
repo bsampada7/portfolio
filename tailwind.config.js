e15a17e53ea4b963b3dc32a0ac738388b5159268
bhujels <bhujelsampada@gmail.com>
2023-03-23 22:41:39 -0400

    new assets and packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: "#1fb6ff",
-        orange: "#ff7849",
-        green: "#13ce66",
-        "gray-dark": "#273444",
-        gray: "#8492a6",
-        "gray-light": "#d3dce6",
-        miami: "#c41230",
-        secondary: "#e000ff",
-        primary: "#8401ff",
-        "primary-light": "#b566ff",
-        "primary-dark": "#7700e6",
-
-        color1: "#EEB4B3",
-        color2: "#C179B9",
-        color3: "#A42CD6",
-        color4: "#502274",
-        color5: "#2F242C",
-
-        "linear-grad": "linear-gradient(#e000ff 0%, #8401ff 100%)",
-
         primary: "#4717F1",
         "primary-dark": "#350cca",
         secondary: "#B417F1",
@@ -42,6 +22,10 @@ module.exports = {
       },
       borderRadius: {
         half: "50%",
+      },
+      screens: {
+        xxs: "300px",
+        xs: "425px",
       },
     },
   },
